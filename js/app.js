558d838b320376e9c467375f4cc8f7fe288e4165
kmj0973 <kmj0973@naver.com>
2023-04-12 02:11:20 +0900

    Save Image, upload image, update css
    update Save Image, upload image, update css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+
 const modeBtn = document.querySelector("#mode-btn");
 const destroyBtn = document.querySelector("#destroy-btn");
 const eraserBtn = document.querySelector("#eraser-btn");
+const saveBtn = document.querySelector("#save");
 const fileBtn = document.querySelector("#file");
+const textInput = document.querySelector("#text");
 
 const canvas = document.querySelector("canvas");
 const lineWidth = document.querySelector("#line-width");
@@ -15,8 +18,11 @@ const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
-
+for(var i=0;i<colorOptions.length;i++){
+    colorOptions[i].style.boxShadow =`-3px 4px 5px ${colorOptions[i].dataset.color}`;
+}   
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 
 let isPainting =false;
 let isFilling = false;
@@ -63,15 +69,18 @@ function onModeClick(event){
 function onCanvasClick(event){
     if(isFilling){
         ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-        filledColor = ctx.fillStyle;
-        
+        filledColor = ctx.fillStyle;       
     }
 }
 function onDestroyClick(event){
     ctx.fillStyle = "white";
     ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    ctx.fillStyle = "black";
     ctx.strokeStyle = "black";
     lineColor.value = "black";
+    isFilling = false;
+    modeBtn.innerText = "Draw";
+    filledColor = "white";
 }
 function onEraserClick(event){
     ctx.strokeStyle = filledColor;
@@ -79,8 +88,9 @@ function onEraserClick(event){
     modeBtn.innerText = "Draw";
 }
 function onFileChange(event){
-    const file = event.target.files[0]; // 파일 접근
-    const url = URL.createObjectURL(file); // url로 변환
+    const file = event.target.files[0]; // 파일 접근 //html속성에 multiple을 넣으면 여러개의 파일을 업로드할 수 있다.
+    const url = URL.createObjectURL(file); // url로 변환, 유저가 업로드한 파일은 이미 브라우저 메모리 안에 있다.
+    // 이 url은 브라우저 내에서만 사용이 가능함.
     const image = new Image();
     image.src = url;
 
@@ -89,6 +99,26 @@ function onFileChange(event){
         fileBtn.value = null;
     }
 }
+function onDoubleClick(event) {
+    const text = textInput.value;
+    if(text != ""){
+    ctx.save(); //ctx의 현재 상태, 색상, 스타일 등 모든 것을 저장함.
+    ctx.lineWidth = 1;
+    ctx.font = "58px serif";
+    ctx.fillText(text, event.offsetX,event.offsetY);
+    ctx.restore(); //save와 restore사이에 일어난 어떠한 수정들은 저장되지 않음.
+    }
+}
+function onSaveClick(event){
+    const url = canvas.toDataURL();//canvas안에 이미지를 url로 변환시켜줌.
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "myDrawing.png";
+    a.click(); //click을 한다는 함수속성이다. fake click을 해주어 이미지를 저장하게 한다.
+}
+
+canvas.addEventListener("dblclick",onDoubleClick);
 canvas.addEventListener("mousedown", onCanvasClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
@@ -101,6 +131,7 @@ lineWidth.addEventListener("change", onLineWidthChange);
 
 colorOptions.forEach(color => color.addEventListener("click",onColorClick));
 
+saveBtn.addEventListener("click", onSaveClick);
 fileBtn.addEventListener("change", onFileChange);
 modeBtn.addEventListener("click",onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
